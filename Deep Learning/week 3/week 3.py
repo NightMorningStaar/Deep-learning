2fc8d6b0f44925a9ad7fef5757d18088097e8e79
NightMorningStaar <64158297+NightMorningStaar@users.noreply.github.com>
2020-08-11 12:16:50 +0530

    Update week 3.py

--- Deep Learning/week 3/week 3.py
+++ Deep Learning/week 3/week 3.py
@@ -10,13 +10,11 @@
 # Package imports
 import numpy as np
 import matplotlib.pyplot as plt
-# from testCases_v2 import *
-from python_prac.testCases_v2 import *
+from testCases_v2 import *
 import sklearn
 import sklearn.datasets
 from sklearn.linear_model import LogisticRegression
-# from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
-from python_prac.planar_utils import plot_decision_boundary,sigmoid,load_planar_dataset,load_extra_datasets
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
 np.random.seed(1) # set a seed so that the results are consistent
 
 
