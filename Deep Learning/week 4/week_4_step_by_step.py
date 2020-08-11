cf9742a81f6680b7652c6ead23760fc7b46fd221
NightMorningStaar <64158297+NightMorningStaar@users.noreply.github.com>
2020-08-11 12:19:02 +0530

    Update week_4_step_by_step.py

--- Deep Learning/week 4/week_4_step_by_step.py
+++ Deep Learning/week 4/week_4_step_by_step.py
@@ -1,10 +1,8 @@
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-#from testCases_v4a import *
-from python_prac.testCases_v4a import *
-#from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
-from python_prac.dnn_utils_v2 import sigmoid,sigmoid_backward,relu,relu_backward
+from testCases_v4a import *
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
 
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
